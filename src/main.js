ea5ef0dcf30151432d3629f9efbcfa76764a1b6e
uilsonfrancisco18 <uilsonfrancisco20@gmail.com>
2026-03-26 23:38:08 -0300

    Primeiro commit do projeto, adicionando arquivos iniciais e configuração básica, preparando o vs code para inicio de intodução das funcionalidades.

---
+++ src/main.js
@@ -0,0 +1,12 @@
+"use strict"
+
+//Importação do dayjs para manipulação de datas
+import "./libs/dayjs.js"
+
+
+
+//Impoortanto todos os arquivos CSS necessários
+import "./styles/global.css"
+import "./styles/form.css"
+import "./styles/schedule.css"
+
